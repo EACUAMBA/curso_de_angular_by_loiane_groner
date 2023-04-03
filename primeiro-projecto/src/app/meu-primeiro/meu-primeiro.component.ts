06d1e6c7fc62854d4f362efbb778c423e9ff47c8
Edilson Alexandre Cuamba <edilsoncuamba@gmail.com>
2023-04-03 23:47:16 +0200

    Curso de Angular #03
    Aprendi a criar um projecto angular;
    Aprendi a criar componentes de forma manual sem o CLI;
    Aprendi a criar componente utilizando o CLI;

---
+++ primeiro-projecto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+  <h1>Meu primeiro component</h1>
+  `
+})
+export class MeuPrimeiroComponent {
+
+}
